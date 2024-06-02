cf79d2fc79f449f42f7472fb4f9d2635b8774b33
atroopa <atroopa@gmail.com>
2024-06-02 15:58:23 -0700

    emit data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex flex-col items-center justify-between pt-10">
     <h1 class="text-red-900 text-3xl font-bold text-center">Open Code Reaction Timer</h1>
-    <button @click="start" :disabled="isPlaying" class="bg-red-900 px-4 py-2 my-3 rounded-2xl text-red-100">Play</button>
+    <button 
+      @click="start" 
+      :disabled="isPlaying"
+      @end="endGame"
+      class="bg-red-900 px-4 py-2 my-3 rounded-2xl text-red-100">Play</button>
     <Block v-if="isPlaying" :delay="delay" />
   </div>
 </template>
+
 <!-- ------------- script --------------- -->
+
 <script>
 import Block from './components/Block.vue'
   export default {
@@ -17,6 +23,7 @@ import Block from './components/Block.vue'
       return {
         delay: 0.0,
         isPlaying : false,
+        score: null,
       }
     },
     methods: {
@@ -24,6 +31,9 @@ import Block from './components/Block.vue'
         this.delay = 2000 + Math.random() * 5000
         this.isPlaying = true
         console.log(this.delay)
+      },
+      endGame(){
+
       }
     }
   }
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,25 +1,34 @@
 <template lang="">
-    <div v-if="showBlock" class="
-     w-[400px] bg-[#0faf87] 
-    text-white p-[100px] 
-    m-10 text-xl 
-    items-center font-bold
-    text-center rounded-xl">
+    <div
+    @click="stopTimer" 
+    v-if="showBlock" 
+    class="
+        w-[400px] bg-[#0faf87] 
+        text-white p-[100px] 
+        m-10 text-xl 
+        items-center font-bold
+        text-center rounded-xl">
         Click me
     </div>
 </template>
+
+<!-- ------------- script --------------- -->
+
 <script>
 export default {
     props : ['delay'],
     data() {
         return {
-            showBlock: false
+            showBlock: false,
+            timer: null,
+            reactionTimer: 0
         }
     },
     mounted(){
         console.log("component mounted");
         setTimeout(()=>{
             this.showBlock = true
+            this.startTimer()
             console.log("after: ", this.delay);
         } ,this.delay)
     },
@@ -29,8 +38,16 @@ export default {
     unmounted() {
         console.log("component unmounted");
     },
+    methods: {
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.reactionTimer += 10
+            }, 10)
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTimer)
+        }
+    },
 }
 </script>
-<style lang="">
-    
-</style>
