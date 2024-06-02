fd0fa0e7e5cebd32ab57804c0e739499e9d38ae3
atroopa <atroopa@gmail.com>
2024-06-02 16:38:21 -0700

    rank add in result component

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       @click="start" 
       :disabled="isPlaying"
       class="bg-red-900 px-4 py-2 my-3 rounded-2xl text-red-100">Play</button>
-      <p class="font-bold text-lg text-green-900 ">Reaction Time: {{score}} mili second</p>
+      <Result v-if="showResult" :score="score"/>
       <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
   </div>
 </template>
@@ -14,26 +14,32 @@
 
 <script>
 import Block from './components/Block.vue'
+import Result from './components/Result.vue'
   export default {
     name : 'App',
     components : {
-      Block
+      Block,
+      Result
     },
     data() {
       return {
         delay: 0.0,
-        isPlaying : false,
+        isPlaying: false,
         score: null,
+        showResult: false
       }
     },
     methods: {
       start(){
         this.delay = 2000 + Math.random() * 5000
         this.isPlaying = true
+        this.showResult = false
         console.log(this.delay)
       },
       endGame(reactionTime){
         this.score = reactionTime
+        this.isPlaying = false
+        this.showResult = true
       }
     }
   }
---
+++ src/components/Result.vue
@@ -0,0 +1,30 @@
+<template lang="">
+    <div>
+        <p  class="font-bold text-lg text-green-900 ">Reaction Time: {{score}} mili second</p>
+        <p>{{rank}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    props: {
+        score: Number
+    }, 
+    mounted() {
+        if(this.score <= 250){
+            this.rank = "عالی بود عالی"
+        } else if(this.score <= 400){
+            this.rank = "ای بدک نبود"
+        } else {
+            this.rank = "گشاد کی بودی تو "
+        }
+    },    
+}
+</script>
+<style lang="">
+    
+</style>
